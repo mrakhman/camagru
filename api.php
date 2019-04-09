d5a594ae5bcd10dc93048d14bf5655fee726f8e8
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-04-09 21:27:29 +0200

    Apr 9 - commented unused part

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once "1_models/camera.model.php";
-include_once "3_controller/save_image_api.control.php";
+//include_once "3_controller/save_image_api.control.php";
 include_once "3_controller/build_post.php";
 include_once "3_controller/delete_post.php";
 include_once "3_controller/like.control.php";
@@ -10,14 +10,14 @@ include_once "3_controller/comment.control.php";
 session_start();
 
 // First if condition is not called anywhere now. It was used during camera building to save raw web photo
-if ($_GET['action']=='save_img')
-{
-    save_img_api();
-}
+//if ($_GET['action']=='save_img')
+//{
+//    save_img_api();
+//}
 
 // It is now substituted by this condition that saves and passes only data of an image
 // Raw image is not stored in database - only post with sticker
-elseif ($_GET['action']=='build_post')
+if ($_GET['action']=='build_post')
 {
     build_post();
 }
