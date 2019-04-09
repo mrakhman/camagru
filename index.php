d78311ec8e2c950f75f8b2cbf12963970116dd6a
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-04-09 21:35:02 +0200

    comment on main page

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 	<body>
 		<?php include_once "2_view/layout/header.php"; ?>
 		<div class="main" align="center">
-            <h3>I need to confirm new CHANGED email address via email !!!</h3>
-            <h4>Do not change email in DB untill new email is confirmed</h4>
+            <h3>Change all website localhost:8080 to http://camagru.ml:8080. NOT change DB localhost <br><br>Changed all I need in users.model except for https:// (now it's not done) <br><br>Need to check other links even though it will not cause damage except for it will be different sessions </h3>
 			<?php include_once "3_controller/register.control.php"; ?><br/>
             <?php include_once "3_controller/friends_posts.control.php"; ?>
 		</div>
